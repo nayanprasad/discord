61263dae9ecd1d703564a56a8f2dffb68eb52637
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-11-28 01:49:10 +0530

    created member management modal and api integrated

--- components/providers/modal-providers.tsx
+++ components/providers/modal-providers.tsx
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from 'react';
 import CreateServerModal from "@/components/modals/create-server-modal";
 import InviteModal from "@/components/modals/invite-modal";
 import EditServerModal from "@/components/modals/edit-server-modal";
+import {MembersModal} from "@/components/modals/members-modal";
 
 
 export const ModalProviders = () => {
@@ -21,6 +22,7 @@ export const ModalProviders = () => {
             <CreateServerModal />
             <InviteModal />
             <EditServerModal />
+            <MembersModal />
         </>
     );
 };
