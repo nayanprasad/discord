062cf9bf9e1353b58e1a08e6906646037b5c471d
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-11-27 20:52:35 +0530

    created page to join the server using invite url

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,52 @@
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { getCurrentProfile } from "@/lib/current-profile";
+
+interface PageProps {
+    params: {
+        inviteCode: string
+    }
+}
+
+const Page = async ({params}: PageProps) => {
+
+    const profile = await getCurrentProfile();
+
+    if (!profile)
+        return redirectToSignIn();
+
+    if(!params.inviteCode)
+        return redirect("/");
+
+    const existingServer = await db.server.findFirst({
+        where: {
+            inviteCode: params.inviteCode,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if (existingServer)
+        return redirect(`/servers/${existingServer.id}`);
+
+    const server = await db.server.update({
+        where: {
+            inviteCode: params.inviteCode
+        },
+        data: {
+            members: {
+                create: {
+                    profileId: profile.id,
+                }
+            }
+        }
+    })
+
+    return null
+};
+
+export default Page;
