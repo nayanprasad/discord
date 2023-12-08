25e6ac11753abd029035b5418035863d28e3b789
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-12-09 00:02:55 +0530

    delete channel modal

---
+++ app/api/channels/[channelId]/route.ts
@@ -0,0 +1,61 @@
+import {getCurrentProfile} from "@/lib/current-profile";
+import {NextResponse} from "next/server";
+import {db} from "@/lib/db";
+import {MemberRole} from "@prisma/client";
+
+export async function DELETE(req: Request, { params }: { params: { channelId: string }}) {
+    try {
+        const profile = await getCurrentProfile();
+        const { searchParams } = new URL(req.url);
+
+        const serverId = searchParams.get("serverId");
+
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if (!serverId) {
+            return new NextResponse("Server ID missing", { status: 400 });
+        }
+
+        if (!params.channelId) {
+            return new NextResponse("Channel ID missing", { status: 400 });
+        }
+
+        console.log("[DELETE_CHANNEL]", serverId, params.channelId)
+
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                members: {
+                    some: {
+                        profileId: profile.id,
+                        role: {
+                            in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+                        }
+                    }
+                }
+            },
+            data: {
+                channels: {
+                    delete: {
+                        id: params.channelId,
+                        name: {
+                            not: "general",
+                        }
+                    }
+                }
+            }
+        });
+
+        return NextResponse.json({
+            success: true,
+            message: "Channel deleted",
+            server
+        }, { status: 200 });
+
+    } catch (error) {
+        console.log("[CHANNEL_DELETE]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
--- components/modals/delete-channel-modal.tsx
+++ components/modals/delete-channel-modal.tsx
@@ -1,35 +1,39 @@
 "use client";
-
 import axios from "axios";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-
 import {
     Dialog,
     DialogContent,
     DialogDescription,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "@/components/ui/button";
+import qs from "query-string";
 
-const DeleteServerModal = () => {
+const DeleteChannelModal = () => {
     const { isOpen, onClose, type, data } = useModal();
     const router = useRouter();
 
-    const isModalOpen = isOpen && type === "deleteServer";
-    const { server } = data;
+    const isModalOpen = isOpen && type === "deleteChannel";
+    const { server, channel } = data;
 
     const [isLoading, setIsLoading] = useState(false);
 
     const handleClick = async () => {
         try {
             setIsLoading(true);
-            await axios.delete(`/api/servers/${server?.id}`);
+            const url = qs.stringifyUrl({
+                url: `/api/channels/${channel?.id}`,
+                query: {
+                    serverId: server?.id
+                }
+            });
+            await axios.delete(url);
             onClose();
-            router.push("/");
+            router.push(`/servers/${server?.id}`);
             router.refresh();
         } catch (error) {
             console.log(error);
@@ -43,11 +47,11 @@ const DeleteServerModal = () => {
             <DialogContent >
                 <DialogHeader >
                     <DialogTitle >
-                        Delete Server
+                        Delete Channel
                     </DialogTitle>
                     <DialogDescription>
                         Are you sure you want to do this? <br />
-                        <span className="text-indigo-500 font-bold">{server?.name}</span> will be permanently deleted.
+                        <span className="text-indigo-500 font-bold">{channel?.name}</span> will be permanently deleted.
                     </DialogDescription>
                 </DialogHeader>
                 <div className="flex justify-between">
@@ -63,4 +67,4 @@ const DeleteServerModal = () => {
     )
 }
 
-export default DeleteServerModal;
+export default DeleteChannelModal;
