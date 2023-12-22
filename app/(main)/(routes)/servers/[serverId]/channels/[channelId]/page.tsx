ae429362cbe09883af7a2a43c27da97fd3e9f0b7
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-12-23 02:05:16 +0530

    created chat header and mobile toggle button

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import {redirectToSignIn} from "@clerk/nextjs";
+import {getCurrentProfile} from "@/lib/current-profile";
+import {db} from "@/lib/db";
+import {redirect} from "next/navigation";
 
-const Page = () => {
+import ChatHeader from "@/components/chat/chat-header";
+
+
+interface ChannelIdPageProps {
+    params: {
+        serverId: string,
+        channelId: string,
+    }
+}
+
+const Page = async ({params}: ChannelIdPageProps) => {
+
+    const profile = await getCurrentProfile();
+
+    if(!profile)
+        return redirectToSignIn();
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        }
+    });
+
+    const member = await db.member.findFirst({
+        where: {
+            profileId: profile.id,
+            serverId: params.serverId,
+        }
+    });
+
+    if(!channel || !member)
+        redirect("/");
+
     return (
-        <div>
-          channel id page
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
         </div>
     );
 };
