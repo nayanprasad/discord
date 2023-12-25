d0d9e3ea1f4b1c33f935053c869f79f309ddc4f8
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-12-25 18:07:40 +0530

    welcome messaage on chat box

---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ChatWelcome from "@/components/chat/chat-welcome";
+import {Member} from "@prisma/client";
+
+
+interface ChatMessagesProps {
+    name: string;
+    member: Member;
+    chatId: string;
+    apiUrl: string;
+    socketUrl: string;
+    socketQuery: Record<string, string>;
+    paramKey: "channelId" | "conversationId";
+    paramValue: string;
+    type: "channel" | "conversation";
+}
+
+const ChatMessages = (
+    {
+        name,
+        member,
+        chatId,
+        apiUrl,
+        socketUrl,
+        socketQuery,
+        paramKey,
+        paramValue,
+        type,
+    }: ChatMessagesProps
+) => {
+    return (
+        <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+            <div className="flex-1"/>
+            <ChatWelcome
+                name={name}
+                type={type}
+            />
+        </div>
+    );
+};
+
+export default ChatMessages;
